a77af9e2a9c56fe4b63c4c41bcd3c6d304df1d59
khushbu joshi <102977739+khushbujoshi2310@users.noreply.github.com>
2022-09-23 11:39:49 +0530

    Update StorageFactory.sol

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,33 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
- 
+
 import "./SimpleStorage.sol";
- 
-contract StorageFactory
-{
- 
+
+contract StorageFactory is SimpleStorage{
+
     SimpleStorage[] public simpleStorageArray;
+
+   function createSimpleStorageContract()public{
+
+       
+       SimpleStorage simpleStorage = new SimpleStorage();
+       simpleStorageArray.push(simpleStorage);
+
+   }
+   function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+
+      SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+     
+
+   }
+
+   function sfGet(uint256 _simpleStorageIndex) public view returns (uint256){
  
-    function createSimpleStorageContract()public
-    {
-        SimpleStorage simplestorage=new SimpleStorage();
-        simpleStorageArray.push(simplestorage);
-    }
- 
-    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public{
- 
-        //Address
-        //ABI
-        SimpleStorage simpleStorage=SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
-        simpleStorage.store(_simpleStorageNumber);
-    }
- 
-    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256)
-    {
-        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
-        // SimpleStorage simpleStorage=SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
-         return simpleStorage.retrieve();
-    }
- 
- 
+     return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+   
+
+   }
+
 }
